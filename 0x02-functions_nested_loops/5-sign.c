2d5c583aa4f963da57a00f15e8774405231d3226
kalebwondimu33 <kalebwondimu33@gmail.com>
2023-07-18 20:35:06 -0700

    the corret answer for question 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,17 @@ int print_sign(int n)
     if (n > 0)
     {
         _putchar('+');
-        return (1);
-    } 
+        return 1;
+    }
     else if (n == 0)
     {
-        _putchar(48);
-        return (0);
-    } 
+        _putchar('0'); /* Changed from 48 to '0' for clarity */
+        return 0;
+    }
     else if (n < 0)
     {
         _putchar('-');
-        return (-1);
+        return -1;
     }
+    return 0;
 }
